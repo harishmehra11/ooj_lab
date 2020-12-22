e5cf21aa438a9be3e844824d67e86a64fdb2eeee
Harish Singh Mehra <73489520+harishmehra11@users.noreply.github.com>
2020-12-22 16:07:38 +0530

    Add files via upload

---
+++ Lab test2/div.java
@@ -0,0 +1,12 @@
+package Arith;
+public class div
+{
+int rem;
+float result;
+public void divcal(int x,int y)
+{
+result=x/y;
+rem=x%y;
+System.out.println("Quotient is :"+result+"Remainder is: "+rem);
+}
+}
